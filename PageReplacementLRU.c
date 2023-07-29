070d0e87a0110c7449eb5dd9c953d9b1c5b085b7
BhoomikaUmesh <119489344+BhoomikaUmesh@users.noreply.github.com>
2023-07-29 06:27:18 +0000

    commit

--- PageReplacementLRU.c
+++ PageReplacementLRU.c
@@ -1,41 +1,69 @@
 #include<stdio.h>
 #include<limits.h>
-int checkHit(int incommingPage, int queue[], int occupied){
+int checkhit(int incomingPage, int queue[], int occupied){
     for(int i=0;i<occupied;i++){
-        if(incommingPage==queue[i])
+        if(incomingPage==queue[i])
         return 1;
     }
     return 0;
 }
-void printFrame(int queue[],int occupied)
-{
+void printFrame(int queue[],int occupied){
     for(int i=0;i<occupied;i++)
-    printf("%d\t\t",queue[i]);
+    printf("%d\t",queue[i]);
 }
-int main()
-{
-    int n=sizeof(incommingStream)/sizeof(incommingSrteam[0]);
-    int frames=3;
-    int queue[n];
-    int distance[n];
-    int occupied=0;
-    int pagefault=0;
+void main(){
+    int incomingStream[]={1,2,3,2,1,5,2,1,6,2,5,6,3,1,3};
+    int n=sizeof(incomingStream)/sizeof(incomingStream[0]);
+    int frames=3,queue[n],distance[n],occupied=0,pagefault=0;
     printf("Page\tFrame1\tFrame2\tFrame3\n");
-    for(int i=0;i<n;i++)
-    {
-        printf("%d:\t\t",incommingStream[i]);
-        //What if currently in Frame 7
-        //next item appears alos 7
-        if(checkHit(incommingStream[i],queue,occupied)){
+    for(int i=0;i<n;i++){
+        printf("%d:\t",incomingStream[i]);
+        if(checkhit(incomingStream[i],queue,occupied)) {
             printFrame(queue,occupied);
-        }
-        //filling when frames are empty
-        else if(occupied<frames){
-            queue[occupied]=incommingStream[i];
-            pageFault++;
+        } else if (occupied<frames){
+            queue[occupied]=incomingStream[i];
+            pagefault++;
             occupied++;
-            
+            printFrame(queue, occupied);
+        } else {
+            int max=INT_MIN,index;
+            for(int j=0;j<frames;j++){
+                distance[j]=0;
+                for(int k=i-1;k>=0;k--){
+                    ++distance[j];
+                    if(queue[j]==incomingStream[k])
+                    break;
+                }
+                if(distance[j]>max){
+                    max=distance[j];
+                    index=j;
+                }
+            }
+            queue[index]=incomingStream[i];
+            printFrame(queue,occupied);
+            pagefault++;
         }
+        printf("\n");
     }
-
+    printf("Page Fault:%d",pagefault);
 }
+
+
+// output
+// Page    Frame1  Frame2  Frame3
+// 1:      1
+// 2:      1       2
+// 3:      1       2       3
+// 2:      1       2       3
+// 1:      1       2       3
+// 5:      1       2       5
+// 2:      1       2       5
+// 1:      1       2       5
+// 6:      1       2       6
+// 2:      1       2       6
+// 5:      5       2       6
+// 6:      5       2       6
+// 3:      5       3       6
+// 1:      1       3       6
+// 3:      1       3       6
+//Page Fault:8
