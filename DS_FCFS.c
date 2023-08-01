b5d80e6ef12b0539142bae003ec162c7177f4781
BhoomikaUmesh <119489344+BhoomikaUmesh@users.noreply.github.com>
2023-08-01 09:25:47 +0000

    commit

---
+++ DS_FCFS.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdilib.h>
+int main()
+{
+    int RQ[100],i,n,TotalHeadMoment=0,initial;
+    printf("Enter the number of Requests\n");
+    scanf("%d",&n);
+    printf("Enter the request sequence\n");
+    for(i=0;i<n;i++)
+    scanf("%d",&RQ[i]);
+    printf("Enter the initial head position\n");
+    scanf("%d",&initial);
+    printf("Sequence of request access:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",RQ[i]);
+        TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+        initial=RQ[i];
+    }
+    printf("\n Total head moment id %d",TotalHeadMoment);
+    return 0;
+}
+
